ae3540b9d23b5aafa9df107729faaa290694bb6a
Rekyt <matthias.grenie@hotmail.com>
2016-10-24 16:34:56 +0200

    Deleted previous data and used Lawing 2015 data
    Deleted previous data. Configured functions using remake to download
    automatically Lawing et al. 2015 data, and compute functional rarity on
    it.

--- remake_scripts/funcs.R
+++ remake_scripts/funcs.R
@@ -1,50 +1,152 @@
-# Functions to be used to rasterize ranges and compute functional rarity over
-# all mammals given by IUCN spatial data using PanTHERIA traits
+# Function to download data from Lawing et al. 2015 doi:10.5061/dryad.9t0n8
 
-# Common Species List between IUCN & PanTHERIA ---------------------------------
-get_common_species = function(iucn_ranges, pantheria_traits) {
+download_lawing_2015 = function() {
+    # Get the handle to download the data from Dryad
+    dryad_handle = "10255/dryad.116171"
 
-    # List of species in common between IUCN range maps and PanTHERIA traits
-    common_species = intersect(iucn_ranges[["binomial"]],
-                               pantheria_traits[["MSW05_Binomial"]])
+    dryad_dir = paste0("data/raw/", strsplit(dryad_handle, "/", fixed = T)[[1]][2],
+                       "/")
 
-    return(common_species)
+
+    # Create corresponding directory
+    dir.create(dryad_dir, recursive = TRUE, showWarnings = FALSE)
+
+    # Archive File Name
+    dryad_archive = paste0(dryad_dir,"DataArchive.zip")
+
+
+    # Create a temporary file
+    tempfile(dryad_archive)
+
+    # Download file
+    dryad_fetch(download_url(dryad_handle), destfile = dryad_archive, mode = "wb")
+
+    # Extract archive
+    unzip(dryad_archive, exdir = substr(dryad_dir, 1, nchar(dryad_dir) - 1))
+
+    # Clean Up Temporary File
+    unlink(dryad_archive)
 }
 
 
-# Return IUCN ranges only for species in common --------------------------------
+# Function to select which trait to include in the analysis based on completeness
+select_traits = function(trait_df) {
+    trait_subset = trait_df %>%
+        filter(!is.na(X5_1_AdultBodyMass_g) & !is.na(X15_1_LitterSize)
+               & !is.na(X6_1_DietBreadth) & !is.na(X6_2_TrophicLevel)
+               & !is.na(X12_1_HabitatBreadth) & !is.na(X12_2_Terrestriality))
 
-common_iucn_ranges = function(iucn_ranges, pantheria_traits) {
-
-    common_species = get_common_species(iucn_ranges, pantheria_traits)
-
-    proj4string(iucn_ranges) = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs "
-
-    return(iucn_ranges[iucn_ranges$binomial %in% common_species,])
+    # Remove all columns containing 'NA'
+    trait_subset[, colSums(is.na(trait_subset)) == 0]
 }
 
 
-# Return PanTHERIA traits only for species in common ---------------------------
+# Function to get same species in presence-absence matrix as trait matrix ------
+get_common_species = function(pres_mat, trait_df) {
 
-common_pan_traits = function(iucn_ranges, pantheria_traits) {
+    mat_columns = colnames(pres_mat)
 
-    common_species = get_common_species(iucn_ranges, pantheria_traits)
+    trait_species = gsub(" ", "_", trait_df[["TaxonName"]])
 
-    return(pantheria_traits[
-        pantheria_traits[["MSW05_Binomial"]] %in% common_species,])
+    # Extract matching species
+    common_species = match(trait_species, mat_columns)
+
+    return(pres_mat[, c(1:3, common_species)])
+}
+
+# Get IUCN statuses ------------------------------------------------------------
+
+get_iucn_status = function(trait_df) {
+
+    species_list = trait_df[["TaxonName"]]
+
+    registerDoParallel(cores = parallel::detectCores())
+
+    # For this function need to have defined a global option 'IUCN_REDLIST_KEY'
+    status = iucn_summary(as.character(species_list), parallel = T) %>%
+        iucn_status()
+
+    stopImplicitCluster()
+
+    # Add IUCN status column in trait df
+    trait_df_status = trait_df %>%
+        inner_join(data.frame(TaxonName = names(status), IUCN_status = status),
+                   by = "TaxonName")
+
+    return(trait_df_status)
 }
 
 
 
-# Rasterize IUCN ranges --------------------------------------------------------
+# Compute Functional Rarity for mammals ----------------------------------------
 
-rasterize_iucn_ranges = function(iucn_ranges, resolution = 3) {
 
-    iucn_raster = raster()
-    res(iucn_raster) = 3
+compute_funrar = function(pres_matrix, trait_df) {
 
-    iucn_raster = rasterize(iucn_ranges, iucn_raster, field = "binomial",
-                            fun = function(x,...)length(na.omit(x)))
+    # Format trait data.frame
+    cleaned_trait_df = format_trait(trait_df)
 
-    return(iucn_raster)
+
+    # Format presence-absence matrix
+    cleaned_pres_mat = format_presence_matrix(pres_matrix)
+
+
+    distance_matrix = compute_dist_matrix(cleaned_trait_df)
+
+    # Get functional rarity indices
+    species_funrar = funrar(cleaned_pres_mat, distance_matrix)
+
+    return(species_funrar)
+}
+
+
+# Clean Up Trait data.frame by sorting traits and ordering them
+format_trait = function(trait_df) {
+
+    # Get species name similar to presence-absence matrix
+    rownames(trait_df) = gsub(" ", "_", trait_df[["TaxonName"]])
+
+    trait_df = trait_df[, -1]  # Take out species name
+
+    # Format traits according to their categories
+    trait_df[["X12_1_HabitatBreadth"]] = factor(
+        trait_df[["X12_1_HabitatBreadth"]], levels = 1:3,
+        labels = c("above_ground_dwelling", "aquatic",
+                   "fossorial_and_ground_dwelling"), ordered = TRUE)
+
+
+    trait_df[["X12_2_Terrestriality"]] = factor(
+        trait_df[["X12_2_Terrestriality"]], levels = c(1, 2),
+        labels = c("fossorial_plus_ground_dwelling", "above_ground_dwelling"),
+        ordered = TRUE)
+
+
+    trait_df[["X6_2_TrophicLevel"]] = factor(trait_df[["X6_2_TrophicLevel"]],
+                                             levels = 1:3, ordered = TRUE,
+                                             labels = c("herbivore",
+                                                        "omnivore",
+                                                        "carnivore"))
+
+    return(trait_df)
+}
+
+# Tidy Up presence-absence matrix by renaming it and converting it to a sparse
+format_presence_matrix = function(pres_mat) {
+
+    rownames(pres_mat) = pres_mat[["GlobalID"]]
+
+    # Take out plot names and coordinates
+    pres_mat = pres_mat[, -c(1:3)]
+
+    pres_mat = as.matrix(pres_mat)
+
+    # Take out species occurring nowhere and sites with no species
+    pres_mat = pres_mat[, which(colSums(pres_mat) != 0)]
+    pres_mat = pres_mat[which(rowSums(pres_mat) != 0),]
+
+    names(dimnames(pres_mat)) = c("Sites", "TaxonName")
+
+    pres_mat = as(pres_mat, "sparseMatrix")
+
+    return(pres_mat)
 }
