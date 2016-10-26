ee0fab5d27992f5aa04a21a477bd499828cfdc0c
Rekyt <matthias.grenie@hotmail.com>
2016-10-26 18:19:04 +0200

    Simpler richness map using default ggplot2

--- remake_scripts/4-make_figures.R
+++ remake_scripts/4-make_figures.R
@@ -10,12 +10,15 @@ draw_richness_map = function(pres_mat) {
 
     site_df = data.frame(site_coords, SpeciesRichness = site_richness)
 
-    richness_map = ggplot(site_df) +
-        geom_tile(aes(x = Longitude, y = Latitude, fill = SpeciesRichness), width = 2) +
-        borders(xlim = range(site_df$Longitude) + c(-2, 2),
-                ylim = range(site_df$Latitude) + c(-2, -2)) +
-        coord_proj(proj = "+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96
-                   +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs ")
+    richness_map = ggplot(site_df, aes(x = Longitude, y = Latitude,
+                                       z = SpeciesRichness)) +
+        stat_summary_2d(bins = 150) +
+        coord_map("albers", at0 = 40, lat1 = 20, xlim = range(site_df$Longitude),
+                  ylim = range(site_df$Latitude)) +
+        theme_map() +
+        theme(legend.position = c(0.1, 0.2))
+
+    return(richness_map)
 }
 
 
